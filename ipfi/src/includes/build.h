85d0ee87314fe4dc6e94ab71b7f08149dcd13a56
Giacomo Strangolino <giacomo.strangolino@elettra.eu>
2026-02-11 21:40:31 +0100

    Makefile -> CMake for ipfi

--- ipfi/src/includes/build.h
+++ ipfi/src/includes/build.h
@@ -1,5 +1,5 @@
 /* Compilation system and compilation date.
- * Written by Makefile (userspace) */
-#define _BUILD_DATE "Wed Feb 11 09:36:51 PM CET 2026"
-#define _BUILD_SYS "Linux 6.18.7-arch1-1 GNU/Linux"
+ * Written by CMake */
+#define _BUILD_DATE "Wed Feb 11 21:40:01 2026"
+#define _BUILD_SYS "Linux 6.18.7-arch1-1 x86_64"
 
